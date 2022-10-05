819cb94c563f2a1a16366fea8c36c07a4da8d5b3
jossuel-js <84153809+jossuel-js@users.noreply.github.com>
2022-10-04 22:18:22 -0300

    Add files via upload

---
+++ tela-Teste/src/componentes/Footer/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  Box,
+
+} from "./styles";
+  
+const Footer = () => {
+  return (
+    <Box>
+      <h1 style={{ color: "#2b13ef", 
+                   textAlign: "center", 
+                    }}>
+        ProjectRecicle
+      </h1>
+      
+    </Box>
+  );
+};
+export default Footer;
