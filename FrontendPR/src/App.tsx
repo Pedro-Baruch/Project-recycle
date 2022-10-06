a58d9f86e2bb323e2297902f12763d69279802b8
jossuel-js <jossueltherock@gmail.com>
2022-10-05 23:04:16 -0300

    Add-Footer+Header / JS  to --> TS

--- FrontendPR/src/App.tsx
+++ FrontendPR/src/App.tsx
@@ -1,10 +1,13 @@
 import { Header } from "./Components/Header-Full/Header"
+
 import './assets/App.css'
+import Footer from "./Components/Footer"
 
 function App() {
 return(
 <main>
 <Header></Header>
+<Footer></Footer>
 </main>)
 }
 
