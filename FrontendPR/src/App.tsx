fdc90b135ac0fcb30cdb84c0b9446763292b7836
jossuel-js <jossueltherock@gmail.com>
2022-10-06 11:25:40 -0300

    Add-ButtonMutavel+css

--- FrontendPR/src/App.tsx
+++ FrontendPR/src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from "./Components/Header-Full/Header"
 import './assets/App.css'
 import Footer from "./Components/Footer"
 import SearchBar from "./Components/Search-Bar/Filter/searchBar"
+import Button from "./Components/Button"
 
 
 
@@ -12,6 +13,9 @@ function App() {
 return(
 <main>
 <Header></Header>
+<Button children = "I'm a button ▶" onClick={() => console.log("You clicked")}/>
+<Button children = "I'm a baruk🃏" onClick={() => console.log("You clicked")}/>
+
 <SearchBar></SearchBar>
 <Footer></Footer>
 </main>)
