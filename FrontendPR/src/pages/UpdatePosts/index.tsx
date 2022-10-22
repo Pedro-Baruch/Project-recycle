87b2d3e3b9644fbab287887665f6e0631109e7f7
jossuel-js <jossueltherock@gmail.com>
2022-10-22 00:55:42 -0300

    alterpage-update

--- FrontendPR/src/pages/UpdatePosts/index.tsx
+++ FrontendPR/src/pages/UpdatePosts/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Button } from 'semantic-ui-react';
 import axios from 'axios';
+import { Content, Div, Form, Input, Label } from './styles';
+import Button from '../../Components/Button';
 
 
 
@@ -31,35 +32,35 @@ export function Update() {
     
     
     return (
-        <div>
+        <Content>
             <Form>
-                <Form.Field>
-                    <label>First Name</label>
-                    <input name="fname"
+                <Div>
+                    <Label style={{paddingBottom:'25px'}}>Title:</Label>
+                    <Input name="name"
                         value={title}
                         onChange={(e) => setTitle(e.target.value)}
                         placeholder='titulo' />
-                </Form.Field>
-                <Form.Field>
-                    <label>Last Name</label>
-                    <input
+                </Div>
+                <Div>
+                    <Label>Description:</Label>
+                    <Input
                         name="lname"
                         value={description}
                         placeholder='descrição'
                         onChange={(e) => setDescription(e.target.value)}
                     />
-                </Form.Field>
-                <Form.Field>
-                    <label>Last Name</label>
-                    <input
+                </Div>
+                <Div>
+                    <Label>Price:</Label>
+                    <Input
                         name="lname"
                         value={price}
                         placeholder='preço'
                         onChange={(e) => setPrice(e.target.value)}
                     />
-                </Form.Field>
-                <Button type='submit' onClick={()=>sendDataToAPI()}>Update</Button>
+                </Div>
+                <Button children='update' height='30px' width='60px' onClick={()=>sendDataToAPI()}/>
             </Form>
-        </div>
+        </Content>
     )
 }
