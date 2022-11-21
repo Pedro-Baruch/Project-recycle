f6014e22dea918eed6461721a831d7eeedc55ba0
Pedro-Baruch <84157648+Pedro-Baruch@users.noreply.github.com>
2022-11-20 22:10:46 -0300

    Update helperApi.tsx

--- FrontendPR/src/hooks/helperApi.tsx
+++ FrontendPR/src/hooks/helperApi.tsx
@@ -1,5 +1,6 @@
-//let token = localStorage.getItem("authToken");
-let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkZmVhMDFiMC04ZGMzLTQyYTktODRhMi02NjllNjczNDI1MGYiLCJ1c2VyUHJvZmlsZUlkIjoiYjk5YzdjNTEtZGI5ZS00NDMxLWFkMmEtMWEzYThjNjA2OGMyIiwiZW1haWwiOiJwZWRyb0Bob3RtYWlsLmNvbSIsImlhdCI6MTY2ODk5MTg4NywiZXhwIjoxNjc5NzkxODg3LCJzdWIiOiJiOTljN2M1MS1kYjllLTQ0MzEtYWQyYS0xYTNhOGM2MDY4YzIifQ.whkLZyJvvvb16MOmXaoDoe9-A2UzLcwf3PBgyOx-Oog"
+let token = localStorage.getItem("authToken");
+//let token = ""
+
 export const config = {
   headers: {
     Authorization: `Bearer ${token}`,
